7e43bd81cd21e8ab2699fe1f2706062b1bd8dd9e
pancake <pancake@nopcode.org>
2018-05-09 21:07:13 +0200

    Test

--- new/bin/r2r.js
+++ new/bin/r2r.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 const NewRegressions = require('..');
-
+console.error('Running the testsuite');
 const fs = require('fs');
 const jsdiff = require('diff');
 const colors = require('colors/safe');
